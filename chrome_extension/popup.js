d25bef41f434836b1560dd25457e5bc5c78a7885
Javier Sánchez T <jefe10jav@gmail.com>
2021-03-17 12:48:55 -0600

    JST - General changes

--- chrome_extension/popup.js
+++ chrome_extension/popup.js
@@ -1,24 +1,24 @@
 
 
-document.getElementById("convert").addEventListener("click", function () {
-    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-        chrome.tabs.sendMessage(tabs[0].id, { type: "getText" }, function (response) {
-            alert(response)
-            //$("#text").text(response);
-            downloadFile("email.html", response);
-        });
-    });
-}); 
+document.getElementById("btn_convert").addEventListener("click", function () {
+ chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+  chrome.tabs.sendMessage(tabs[0].id, { type: "getText" }, function (response) {
+   //alert(response)
+   //$("#text").text(response);
+   downloadFile("email.html", response);
+  });
+ });
+});
 
 
 function downloadFile(filename, text) {
-    var element = document.createElement('a');
-    element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
-    element.setAttribute('download', filename);
-   
-    element.style.display = 'none';
-    document.body.appendChild(element);
-    element.click();
-   
-    document.body.removeChild(element);
-   }
+ var element = document.createElement('a');
+ element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
+ element.setAttribute('download', filename);
+
+ element.style.display = 'none';
+ document.body.appendChild(element);
+ element.click();
+
+ document.body.removeChild(element);
+}
