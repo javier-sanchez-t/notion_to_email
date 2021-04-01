702a99947162847564ef9db43e8068fc13d04283
Javier Sánchez T <jefe10jav@gmail.com>
2021-03-31 23:53:00 -0600

    JST Changes for standalone

--- chrome_extension/popup.js
+++ chrome_extension/popup.js
@@ -1,24 +1,26 @@
-
-
 document.getElementById("btn_convert").addEventListener("click", function () {
- chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-  chrome.tabs.sendMessage(tabs[0].id, { type: "getText" }, function (response) {
-   //alert(response)
-   //$("#text").text(response);
-   downloadFile("email.html", response);
+  chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+    chrome.tabs.sendMessage(tabs[0].id, { type: "getText" }, function (response) {
+      /*if(!response || response.includes('error')){
+        swal("😮 Ups!", "Something is wrong! \n Please be sure you are using a Notion page.", "error");
+      }*/
+      
+      if (response && !response.includes('error')) {
+        downloadFile("Email.html", response);
+      }
+    });
   });
- });
 });
 
-
 function downloadFile(filename, text) {
- var element = document.createElement('a');
- element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
- element.setAttribute('download', filename);
+  var element = document.createElement('a');
+  element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
+  element.setAttribute('download', filename);
 
- element.style.display = 'none';
- document.body.appendChild(element);
- element.click();
+  element.style.display = 'none';
+  document.body.appendChild(element);
+  element.click();
 
- document.body.removeChild(element);
+  document.body.removeChild(element);
 }
+
